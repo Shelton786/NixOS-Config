c8b62eb1997380cfcc4faebba647424dad1b34ae
HenryZeng <zengdeveloper@qq.com>
2024-08-11 08:54:53 +0800

    init

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "HenryZeng 's NixOS Flake";
+
+  inputs = {
+    nixpkgs.url = "https://mirrors.ustc.edu.cn/nix-channels/nixos-24.05/nixexprs.tar.xz";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations = {
+        "ASUS_TianXuan4_Rikki" = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; }; # this is the important part
+          modules = [
+            ./device/ASUS_TianXuan4
+            ./global/laptop-dev-env/default.nix
+            # ./input/language.nix
+            # home-manager.nixosModules.home-manager
+            # {
+            #   home-manager.useGlobalPkgs = true;
+            #   home-manager.useUserPackages = true;
+            #   home-manager.users.HenryZeng = import ./users/henryzeng.nix;
+            #   home-manager.extraSpecialArgs = inputs;
+            # }
+          ];
+      };
+    };
+  };
+}
