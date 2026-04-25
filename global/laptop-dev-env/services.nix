30608f5009ae069c9a04744114d34db5449242ae
Shelton786 <1398473754@qq.com>
2026-04-25 10:41:07 +0800

    change:v2raya更换为v2rayn

--- global/laptop-dev-env/services.nix
+++ global/laptop-dev-env/services.nix
@@ -1,3 +1,15 @@
+{ pkgs, ... }:
 {
-  services.v2raya.enable = true;
+  #services.v2raya.enable = true;
+  
+  # 添加 v2rayn
+  environment.systemPackages = with pkgs; [
+    v2rayn
+    # 建议同时保留 networkmanager，v2rayN 依赖它来管理系统代理
+    #networkmanager 
+  ];
+
+  # 确保开启了 polkit，否则 v2rayN 申请管理员权限设置系统代理时会报错
+  security.polkit.enable = true;
+
 }
