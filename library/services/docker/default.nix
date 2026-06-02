7801f69cd7b36f638284f3fc903a97f8011d1371
Shelton786 <1398473754@qq.com>
2026-06-02 14:10:54 +0800

    change:本地代理端口更新

--- library/services/docker/default.nix
+++ library/services/docker/default.nix
@@ -4,8 +4,8 @@
   virtualisation.docker.storageDriver = "btrfs";
   virtualisation.docker.daemon.settings = {
     proxies = {
-      http-proxy = "http://127.0.0.1:20171";
-      https-proxy = "http://127.0.0.1:20171";
+      http-proxy = "http://127.0.0.1:20170";
+      https-proxy = "http://127.0.0.1:20170";
       no-proxy = "*.test.example.com,.example.org,127.0.0.0/8";
     };
   };
